3b0a0279f0d9202ea106cb31116854a2dbf1d65e
AlexSzklarski <alexszklarski7@gmail.com>
2026-06-03 15:14:07 +0200

    Test

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -6,6 +6,12 @@ output "id_hub_vpc" {
     description = "Id of the hub VPC."
 }
 
+output "id_pub_hub_subnet" {
+    value = module.vpc_module.hub_vpc.public_subnets
+
+    description = "Id of the public subnet 0 on the hub VPC."
+}
+
 output "id_pub_hub_subnet_0" {
     value = module.vpc_module.hub_vpc.public_subnets[0]
 
