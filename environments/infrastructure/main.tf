93e12aa7d7b7122ff54eb983a6d37d5907fe4495
AlexSzklarski <alexszklarski7@gmail.com>
2026-06-03 14:55:25 +0200

    Test

--- environments/infrastructure/main.tf
+++ environments/infrastructure/main.tf
@@ -86,12 +86,12 @@ module "network" {
     tgw_attach = {
         hub_attachement = {
             vpc_id = data.aws_vpc.hub_vpc.id
-            subnet_ids = module.network.id_pub_hub_subnet
+            subnet_ids = [module.network.id_pub_hub_subnet]
             destination_cidr_block = data.aws_vpc.spoke_vpc.cidr_block
         },
         spoke_attachement = {
             vpc_id = data.aws_vpc.spoke_vpc.id
-            subnet_ids = module.network.id_priv_spoke_subnet
+            subnet_ids = [module.network.id_priv_spoke_subnet]
             destination_cidr_block = data.aws_vpc.hub_vpc.cidr_block
         }
     }
