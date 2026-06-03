986f85d93e690707fafca062831909dc1d9ea568
AlexSzklarski <alexszklarski7@gmail.com>
2026-06-03 20:21:27 +0200

    Test

--- modules/network/sg/output.tf
+++ modules/network/sg/output.tf
@@ -1,20 +1,20 @@
 ## Security Group Outputs
 output "alb_sg_id" {
-    value = module.security_groups.alb-sg.security_group_id
+    value = module.hub_security_groups.alb-sg.security_group_id
     depends_on = [ module.hub_security_groups ]
 
     description = "Id of the Application Load Balancer (ALB) security group."
 }
 
 output "eks_sg_id" {
-    value = module.security_groups.eks_sg.security_group_id
+    value = module.spoke_security_groups.eks_sg.security_group_id
     depends_on = [ module.spoke_security_groups ]
 
     description = "Id of the Elastic Kubernetes Service (EKS) security group."
 }
 
 output "rds_sg_id" {
-    value = module.security_groups["rds_sg"].security_group_id
+    value = module.spoke_security_groups.rds_sg.security_group_id
     depends_on = [ module.spoke_security_groups ]
 
     description = "Id of the Relational Database Service (RDS) security group."
