674b9e1f48e5064976477cdf86f7d7f689383c1a
AlexSzklarski <alexszklarski7@gmail.com>
2026-06-04 00:47:53 +0200

    Fixed Helm provider error

--- environments/infrastructure/providers.tf
+++ environments/infrastructure/providers.tf
@@ -1,5 +1,5 @@
 provider "helm" {
-    kubernetes {
+    kubernetes = {
       host = module.eks.eks_cluster_endpoint
       cluster_ca_certificate = base64decode(module.eks.eks_cluster_ca)
       token = data.aws_eks_cluster_auth.eks_cluster_data.token
