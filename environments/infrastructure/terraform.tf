5709275be32540de6f2631aeff55fabe5c090950
AlexSzklarski <alexszklarski7@gmail.com>
2026-06-04 06:18:44 +0200

    AWS Apply S3 bucket

--- environments/infrastructure/terraform.tf
+++ environments/infrastructure/terraform.tf
@@ -10,10 +10,4 @@ terraform {
         version = "~> 2.0"
     }
   }
-
-  backend "s3" {
-    bucket = "cicd-bucket-huby71"
-    key = "env/dev/terraform.tfstate"
-    region = "eu-central-1"
-  }
 }
