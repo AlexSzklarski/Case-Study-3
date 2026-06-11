a867f58ac6b9b3248f597a8bea2a124463f2bdca
AlexSzklarski <alexszklarski7@gmail.com>
2026-06-11 10:21:12 +0200

    .

--- environments/infrastructure/data.tf
+++ environments/infrastructure/data.tf
@@ -1,62 +1,62 @@
-## Compute Module Datablocks
-data "aws_eks_cluster_auth" "eks_cluster_data" {
-  name = "eks_cluster"
-}
+# ## Compute Module Datablocks
+# data "aws_eks_cluster_auth" "eks_cluster_data" {
+#   name = "eks_cluster"
+# }
 
-## Network Module Datablocks
-data "aws_vpc" "hub_vpc" {
-    filter {
-      name = "vpc-id"
-      values = [ module.vpc.id_hub_vpc ]
-    }
-}
+# ## Network Module Datablocks
+# data "aws_vpc" "hub_vpc" {
+#     filter {
+#       name = "vpc-id"
+#       values = [ module.vpc.id_hub_vpc ]
+#     }
+# }
 
-data "aws_subnets" "public_subnets" {
-    filter {
-      name = "vpc-id"
-      values = [ module.vpc.id_hub_vpc ]
-    }
+# data "aws_subnets" "public_subnets" {
+#     filter {
+#       name = "vpc-id"
+#       values = [ module.vpc.id_hub_vpc ]
+#     }
 
-    filter {
-        name = "tag:Type"
-        values = ["public"]
-    }
-}
+#     filter {
+#         name = "tag:Type"
+#         values = ["public"]
+#     }
+# }
 
-data "aws_vpc" "spoke_vpc" {
-    filter {
-      name = "vpc-id"
-      values = [ module.vpc.id_spoke_vpc ]
-    }
-}
+# data "aws_vpc" "spoke_vpc" {
+#     filter {
+#       name = "vpc-id"
+#       values = [ module.vpc.id_spoke_vpc ]
+#     }
+# }
 
-data "aws_subnets" "private_subnets" {
-    filter {
-      name = "vpc-id"
-      values = [ module.vpc.id_spoke_vpc ]
-    }
+# data "aws_subnets" "private_subnets" {
+#     filter {
+#       name = "vpc-id"
+#       values = [ module.vpc.id_spoke_vpc ]
+#     }
 
-    filter {
-        name = "tag:Type"
-        values = ["private"]
-    }
+#     filter {
+#         name = "tag:Type"
+#         values = ["private"]
+#     }
 
-    depends_on = [ module.vpc ]
-}
+#     depends_on = [ module.vpc ]
+# }
 
-data "aws_subnets" "database_subnets" {
-    filter {
-      name = "vpc-id"
-      values = [ module.vpc.id_spoke_vpc ]
-    }
+# data "aws_subnets" "database_subnets" {
+#     filter {
+#       name = "vpc-id"
+#       values = [ module.vpc.id_spoke_vpc ]
+#     }
 
-    filter {
-        name = "tag:Type"
-        values = ["database"]
-    }
+#     filter {
+#         name = "tag:Type"
+#         values = ["database"]
+#     }
 
-    depends_on = [ module.vpc ]
-}
+#     depends_on = [ module.vpc ]
+# }
 
 data "aws_alb" "alb_url" {
   name = "hub-alb"
