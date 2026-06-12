b4cb16319723ed0a3cf08983e9993692a24d8da9
AlexSzklarski <alexszklarski7@gmail.com>
2026-06-12 13:25:31 +0200

    Added RDS

--- environments/infrastructure/data.tf
+++ environments/infrastructure/data.tf
@@ -3,63 +3,63 @@
 #   name = "eks_cluster"
 # }
 
-# ## Network Module Datablocks
-# data "aws_vpc" "hub_vpc" {
-#     filter {
-#       name = "vpc-id"
-#       values = [ module.vpc.id_hub_vpc ]
-#     }
-# }
+## Network Module Datablocks
+data "aws_vpc" "hub_vpc" {
+    filter {
+      name = "vpc-id"
+      values = [ module.vpc.id_hub_vpc ]
+    }
+}
 
-# data "aws_subnets" "public_subnets" {
-#     filter {
-#       name = "vpc-id"
-#       values = [ module.vpc.id_hub_vpc ]
-#     }
+data "aws_subnets" "public_subnets" {
+    filter {
+      name = "vpc-id"
+      values = [ module.vpc.id_hub_vpc ]
+    }
 
-#     filter {
-#         name = "tag:Type"
-#         values = ["public"]
-#     }
-# }
+    filter {
+        name = "tag:Type"
+        values = ["public"]
+    }
+}
 
-# data "aws_vpc" "spoke_vpc" {
-#     filter {
-#       name = "vpc-id"
-#       values = [ module.vpc.id_spoke_vpc ]
-#     }
-# }
+data "aws_vpc" "spoke_vpc" {
+    filter {
+      name = "vpc-id"
+      values = [ module.vpc.id_spoke_vpc ]
+    }
+}
 
-# data "aws_subnets" "private_subnets" {
-#     filter {
-#       name = "vpc-id"
-#       values = [ module.vpc.id_spoke_vpc ]
-#     }
+data "aws_subnets" "private_subnets" {
+    filter {
+      name = "vpc-id"
+      values = [ module.vpc.id_spoke_vpc ]
+    }
 
-#     filter {
-#         name = "tag:Type"
-#         values = ["private"]
-#     }
+    filter {
+        name = "tag:Type"
+        values = ["private"]
+    }
 
-#     depends_on = [ module.vpc ]
-# }
+    depends_on = [ module.vpc ]
+}
 
-# data "aws_subnets" "database_subnets" {
-#     filter {
-#       name = "vpc-id"
-#       values = [ module.vpc.id_spoke_vpc ]
-#     }
+data "aws_subnets" "database_subnets" {
+    filter {
+      name = "vpc-id"
+      values = [ module.vpc.id_spoke_vpc ]
+    }
 
-#     filter {
-#         name = "tag:Type"
-#         values = ["database"]
-#     }
+    filter {
+        name = "tag:Type"
+        values = ["database"]
+    }
 
-#     depends_on = [ module.vpc ]
-# }
+    depends_on = [ module.vpc ]
+}
 
-# data "aws_alb" "alb_url" {
-#   name = "hub-alb"
+data "aws_alb" "alb_url" {
+  name = "hub-alb"
 
-#   depends_on = [ module.alb ]
-# }
+  depends_on = [ module.alb ]
+}
--- environments/infrastructure/main.tf
+++ environments/infrastructure/main.tf
@@ -40,92 +40,92 @@
 #     iam_role_vars = var.iam_role_vars
 # }
 
-# module "rds" {
-#     source = "../../modules/database/rds"
+module "rds" {
+    source = "../../modules/database/rds"
 
-#     ## RDS Variables
-#     rds_vars = var.rds_vars
-#     rds_subnet_ids = module.vpc.id_db_spoke_subnet
-#     rds_sg_id = [module.sg.rds_sg_id]
-# }
+    ## RDS Variables
+    rds_vars = var.rds_vars
+    rds_subnet_ids = module.vpc.id_db_spoke_subnet
+    rds_sg_id = [module.sg.rds_sg_id]
+}
 
-# module "vpc" {
-#     source = "../../modules/network/vpc"
+module "vpc" {
+    source = "../../modules/network/vpc"
 
-#     ## VPC Variables
-#     vpc_vars = var.vpc_vars
-# }
+    ## VPC Variables
+    vpc_vars = var.vpc_vars
+}
 
-# module "sg" {
-#     source = "../../modules/network/sg"
+module "sg" {
+    source = "../../modules/network/sg"
 
-#     ## Hub Security Group Variables
-#     sg_hub_vars = var.hub_sg_vars
-#     sg_hub_vpc_id = data.aws_vpc.hub_vpc.id
+    ## Hub Security Group Variables
+    sg_hub_vars = var.hub_sg_vars
+    sg_hub_vpc_id = data.aws_vpc.hub_vpc.id
 
-#     ## Spoke Security Group Variables
-#     sg_spoke_vars = var.spoke_sg_vars
-#     sg_spoke_vpc_id = data.aws_vpc.spoke_vpc.id
-# }
+    ## Spoke Security Group Variables
+    sg_spoke_vars = var.spoke_sg_vars
+    sg_spoke_vpc_id = data.aws_vpc.spoke_vpc.id
+}
 
-# module "alb" {
-#     source = "../../modules/network/alb"
+module "alb" {
+    source = "../../modules/network/alb"
 
-#     ## ALB Variables
-#     alb_vars = var.alb_vars
-#     alb_subnets = [module.vpc.id_pub_hub_subnet_0, module.vpc.id_pub_hub_subnet_1]
-#     alb_vpc_id = data.aws_vpc.hub_vpc.id
-#     alb_security_group = [module.sg.alb_sg_id]
+    ## ALB Variables
+    alb_vars = var.alb_vars
+    alb_subnets = [module.vpc.id_pub_hub_subnet_0, module.vpc.id_pub_hub_subnet_1]
+    alb_vpc_id = data.aws_vpc.hub_vpc.id
+    alb_security_group = [module.sg.alb_sg_id]
 
-#     # target_group_vars = {   
-#     #     nginx-target = {
-#     #         name = "nginx-target"
-#     #         port = 80
-#     #         protocol = "HTTP"
-#     #         target_type = "ip"
-#     #         target_id = ""
-#     #         availability_zone = "eu-central-1a"
-#     #         create_attachment = false
+    # target_group_vars = {   
+    #     nginx-target = {
+    #         name = "nginx-target"
+    #         port = 80
+    #         protocol = "HTTP"
+    #         target_type = "ip"
+    #         target_id = ""
+    #         availability_zone = "eu-central-1a"
+    #         create_attachment = false
 
-#     #         health_check = {
-#     #             path = "/"
-#     #             port = "80"
-#     #             protocol = "HTTP"
-#     #             matcher = "200"
-#     #         }
-#     #     }
-#     # }
+    #         health_check = {
+    #             path = "/"
+    #             port = "80"
+    #             protocol = "HTTP"
+    #             matcher = "200"
+    #         }
+    #     }
+    # }
 
-#     # listener_vars = {
-#     #     nginx-listener = {
-#     #         port = 80
-#     #         protocol = "HTTP"
-#     #         forward = {
-#     #             target_group_key = "nginx-target"
-#     #         }
-#     #     }
-#     # }
-# }
+    # listener_vars = {
+    #     nginx-listener = {
+    #         port = 80
+    #         protocol = "HTTP"
+    #         forward = {
+    #             target_group_key = "nginx-target"
+    #         }
+    #     }
+    # }
+}
 
-# module "tgw" {
-#     source = "../../modules/network/tgw"
+module "tgw" {
+    source = "../../modules/network/tgw"
     
-#     ## TGW Variables
-#     tgw_vars = var.tgw_vars
+    ## TGW Variables
+    tgw_vars = var.tgw_vars
 
-#     tgw_attach = {
-#         hub_attachement = {
-#             vpc_id = data.aws_vpc.hub_vpc.id
-#             subnet_ids = [module.vpc.id_pub_hub_subnet_0, module.vpc.id_pub_hub_subnet_1]
-#             destination_cidr_block = data.aws_vpc.spoke_vpc.cidr_block
-#         },
-#         spoke_attachement = {
-#             vpc_id = data.aws_vpc.spoke_vpc.id
-#             subnet_ids = [module.vpc.id_priv_spoke_subnet_0]
-#             destination_cidr_block = data.aws_vpc.hub_vpc.cidr_block
-#         }
-#     }
-# }
+    tgw_attach = {
+        hub_attachement = {
+            vpc_id = data.aws_vpc.hub_vpc.id
+            subnet_ids = [module.vpc.id_pub_hub_subnet_0, module.vpc.id_pub_hub_subnet_1]
+            destination_cidr_block = data.aws_vpc.spoke_vpc.cidr_block
+        },
+        spoke_attachement = {
+            vpc_id = data.aws_vpc.spoke_vpc.id
+            subnet_ids = [module.vpc.id_priv_spoke_subnet_0]
+            destination_cidr_block = data.aws_vpc.hub_vpc.cidr_block
+        }
+    }
+}
 
 ## Cognito user pool which saves users.
 resource "aws_cognito_user_pool" "website_user_pool" {
