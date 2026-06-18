ab7ce329691e59ba176ace36f7f8fd6cf926ab14
AlexSzklarski <alexszklarski7@gmail.com>
2026-06-18 05:04:43 +0200

    .

--- environments/infrastructure/data.tf
+++ environments/infrastructure/data.tf
@@ -18,25 +18,25 @@ data "aws_ami" "ovpn_ami" {
     }
 }
 
-## Network Module Datablocks
-data "aws_vpc" "hub_vpc" {
-    filter {
-      name = "vpc-id"
-      values = [ module.vpc.id_hub_vpc ]
-    }
-}
+# ## Network Module Datablocks
+# data "aws_vpc" "hub_vpc" {
+#     filter {
+#       name = "vpc-id"
+#       values = [ module.vpc.id_hub_vpc ]
+#     }
+# }
 
-data "aws_subnets" "public_subnets" {
-    filter {
-      name = "vpc-id"
-      values = [ module.vpc.id_hub_vpc ]
-    }
+# data "aws_subnets" "public_subnets" {
+#     filter {
+#       name = "vpc-id"
+#       values = [ module.vpc.id_hub_vpc ]
+#     }
 
-    filter {
-        name = "tag:Type"
-        values = ["public"]
-    }
-}
+#     filter {
+#         name = "tag:Type"
+#         values = ["public"]
+#     }
+# }
 
 data "aws_vpc" "spoke_vpc" {
     filter {
@@ -73,8 +73,8 @@ data "aws_subnets" "database_subnets" {
     depends_on = [ module.vpc ]
 }
 
-data "aws_alb" "alb_url" {
-  name = "hub-alb"
+# data "aws_alb" "alb_url" {
+#   name = "hub-alb"
 
-  depends_on = [ module.alb ]
-}
+#   depends_on = [ module.alb ]
+# }
